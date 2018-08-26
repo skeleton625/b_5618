518a8c1332097fede4d70c0aea950d6885320e92
WaniFi <skelpd24@gmail.com>
2018-08-26 21:33:49 +0900

    5618 문제 풀이 개선 완료

--- baekjoon_5618/main.cpp
+++ baekjoon_5618/main.cpp
@@ -1,17 +1,25 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
-int n[3], t, c, m = 100000000;
-int main() {
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		cin >> n[i];
-		m = m > n[i] ? n[i] : m;
+int n[3], t, c;
+int gcd(int a, int b) {
+	int m, n, tmp;
+	m = a > b ? a : b;
+	n = a < b ? a : b;
+	while (m%n != 0) {
+		tmp = n;
+		n = m % n;
+		m = tmp;
 	}
-	for (int i = 1; i <= m; i++) {
-		for (int j = 0; j < t; j++) if (n[j] % i == 0) c++;
-		if (c == t) cout << i << endl;
-		c = 0;
+	return n;
+}
+
+int main() {
+	scanf_s("%d", &t);
+	for (int i = 0; i < t; i++) scanf_s("%d",&n[i]);
+	if (t == 2) c = gcd(n[0], n[1]);
+	else c = gcd(gcd(n[0], n[1]), n[2]);
+	for (int i = 1; i <= c; i++) {
+		if (c%i==0) printf("%d\n", i);
 	}
 	return 0;
 }
